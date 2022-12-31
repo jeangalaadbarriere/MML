0dac3fc4685c8403200e9137f70697a46702fcab
Flavien Gervois <flavien.gervois@gmail.com>
2022-12-31 13:02:49 +0100

    Redaction de la première partie Implémentation de la 3-pass-method

--- traitement_portfolios.R
+++ traitement_portfolios.R
@@ -12,7 +12,7 @@ for(f in list_files){
   data<-readLines(paste0("rawdata/", f))
   data<-gsub(",  ", ",", data)
   data<-gsub(", ", ",", data) # on enleve les espaces inutiles entre cellules
-  data<-c(paste(1:31, collapse = ","),"\n", data) # on a au maximum 30 portfolios
+  data<-c(paste(1:40, collapse = ","),"\n", data) # on a au maximum 30 portfolios
 
   write.table(data,paste0("first_treatment/", f), row.names = F,col.names=F, quote=F)
   # quote = F pour éviter de mettre des guillemets partout!
@@ -85,4 +85,29 @@ for(f in list_files){
   saveRDS(data,paste0("second_treatment/", f, ".rds"))
 
 }
+# ensuite on copie tout à la main dans le dossier "portfolios_treated"
 
+############### regrouper les portfolios sur un même fichier
+list_files <- list.files("data/portfolios/second_treatment")
+portfolios <- vector(, length = 711)
+for(f in list_files){
+  tmp <- readRDS(paste0("data/portfolios/second_treatment/", f))
+  print(ncol(tmp))
+  # portfolios à 20 colonnes doivent être réduits à 10
+  if(ncol(tmp)==20){
+    tmp<-dplyr::select(tmp, c(1,11:20))
+  }
+  tmp <- dplyr::select(filter(tmp,date>='1963-07-01'),-1) # on enlève la colonne 'date'
+  portfolios <- cbind(portfolios, tmp)
+  
+}
+# dates :
+tmp <- readRDS(paste0("data/portfolios/second_treatment/", f))
+dates <- dplyr::select(filter(tmp,date>='1963-07-01'),1)
+
+portfolios<-portfolios[,-1]
+portfolios<-cbind(dates, portfolios)
+
+saveRDS(portfolios, "data/portfolios.RDS")
+
+# les fichiers censés avoir 10 portfolios en ont en réalité 19         
